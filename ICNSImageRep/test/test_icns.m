9a6137045b1cf8d67c253baf68de1b60ec47e67f
probonopd <probonopd@users.noreply.github.com>
2026-01-03 13:17:06 +0100

    Initial implementation of NSICNSImageRep

---
+++ ICNSImageRep/test/test_icns.m
@@ -0,0 +1,46 @@
+/*
+ * Test program for ICNS loading
+ */
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    if (argc < 2)
+    {
+        printf("Usage: %s <icns-file>\n", argv[0]);
+        [pool release];
+        return 1;
+    }
+    
+    NSString *path = [NSString stringWithUTF8String:argv[1]];
+    
+    NSLog(@"Attempting to load ICNS file: %@", path);
+    
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    
+    if (!data)
+    {
+        NSLog(@"✗ Failed to read file");
+        [pool release];
+        return 1;
+    }
+    
+    NSLog(@"✓ Successfully read file: %lu bytes", (unsigned long)[data length]);
+    
+    if ([data length] >= 4)
+    {
+        const unsigned char *bytes = [data bytes];
+        NSLog(@"  Magic: '%c%c%c%c'", bytes[0], bytes[1], bytes[2], bytes[3]);
+        
+        if (bytes[0] == 'i' && bytes[1] == 'c' && bytes[2] == 'n' && bytes[3] == 's')
+        {
+            NSLog(@"✓ File is a valid ICNS file");
+        }
+    }
+    
+    [pool release];
+    return 0;
+}
